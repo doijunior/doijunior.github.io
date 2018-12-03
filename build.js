0cc6413bb77a7e1d06a0e70a09468d4bfc2feccd
Gilson <doijunior@yahoo.com.br>
2018-12-03 18:44:11 -0200

    new layotu from scratch

---
+++ build.js
@@ -0,0 +1,9 @@
+var ejs = require('ejs');
+var fs = require('fs');
+
+var template = fs.readFileSync('./components/template.ejs', 'utf8');
+var social = fs.readFileSync('./components/social.ejs', 'utf8');
+
+var templateCompiled = ejs.render(template, {social: social});
+
+fs.writeFileSync('index2.html', templateCompiled);
